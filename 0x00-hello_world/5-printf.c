b65ab491e11d13b5840127dc50c5d4263cf12238
Emmanuel-Shivona <emmanuelshivona1@gmail.com>
2024-06-19 03:59:28 -0700

    add file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,24 @@
+/**
+ *Name: 5-printf.c
+ *Author: Emmanuel-Shivona
+ *Description - The program send a specific message to the output
+ */
+
+/**
+ *Name: main
+ *Return value: returns 0 if program runs successfully
+ *
+ *Description - The program's entry point
+ */
+#include <stdio.h>
+int main(void)
+{
+/**
+ *Name: printf
+ *Return value: Returns the specified message
+ *
+ *Description - The function outputs the specified message to the standard output
+ */
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
