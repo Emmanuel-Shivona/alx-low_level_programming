fe4bcaa81c8170a1298bb9427a21ef9f35bb4466
Emmanuel-Shivona <emmanuelshivona1@gmail.com>
2024-07-25 05:07:03 -0700

    modify file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,18 @@
 /**
- * File: 4-puts.c
+ * File - 4-puts.c
  * Author: Emmanuel-Shivona
+ *
  * Description: The program sends a message to the output
  */
 #include <stdio.h>
 /**
  * main - Entry point of the program
  *
- *
  * Description: Prints a specific message to the console
  * Return: 0 if success, otherwise if failure
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
