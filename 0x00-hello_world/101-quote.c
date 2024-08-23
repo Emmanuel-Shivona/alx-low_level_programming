f818bb4daa7ced016c256633285ebc5e2e5ded3d
Emmanuel-Shivona <emmanuelshivona1@gmail.com>
2024-08-23 02:02:01 -0700

    modify file 101-

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <unistd.h>
-
+/**
+ *main - entry point of the program
+ *
+ *Description: The entry point of the program
+ *Return: 1 if unsuccessful, 0 if successful
+ */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59);
-    return 1;
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10
+-19\n";
+write(2, message, 59)
+return (1);
 }
